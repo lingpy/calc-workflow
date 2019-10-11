d5baf0810972a37ba3095c09e9267e653e8a131a
MacyL <wumeishin@gmail.com>
2019-10-11 15:09:35 +0200

    create subset

--- workflow/1_lifting.py
+++ workflow/1_lifting.py
@@ -3,40 +3,26 @@ from lingpy import *
 
 
 wl = Wordlist.from_cldf(
-        ds().dir.joinpath('cldf','cldf-metadata.json').as_posix(),
-        col="language_id",
-        row="parameter_id",
-        )
-
-wl.add_entries('doculect', 'language_name', lambda x: x)
-wl.add_entries('concept', 'concept_name', lambda x: x)
+        ds().dir.joinpath('cldf','cldf-metadata.json'))
 
 languages = [
-        "Luobuohe, Western",
+        "EasternLuobuohe",
         "Chuanqiandian",
-        "Chuanqiandian, Central Guizhou",
-        "Xiangxi, West",
-        "Xiangxi, East",
+        "CentralGuizhouChuanqiandian",
+        "WesternXiangxi",
+        "EasternXiangxi",
         "Bana",
         "Younuo",
         "Numao"
-        "Baheng, East",
-        "Baheng, West",
-        "Qiandong, East",
-        "Qiandong, West",
-        "Biao Min",
-        "Zao Min"] # modify
+        "EasternBahen",
+        "WesternBaheng",
+        "EasternQiandong",
+        "WesternQiandong",
+        "'BiaoMin",
+        "ZaoMin"] # modify
 
-#wl.add_entries('new_segments', 'segments', lambda x: x[:-1])
-wl.add_entries('ipa', 'segments', lambda x: ''.join(x))
-
-wl.output('tsv', filename='Chen_subset',
+wl.output('tsv', filename='D_Chen_subset',
         prettify=False,
         subset=True,
         rows=dict(doculect='in '+str(languages))
         )
-
-#for idx, doculect, concept, tokens in wl.iter_rows('doculect', 'concept',
-#        'segments'):
-
-# sinopy profile  WORDLIST --column form -o ../etc/profile2.tsv
