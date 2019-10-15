4d33b831593e0995c6aae7459c66bbdffc04879c
MacyL <wumeishin@gmail.com>
2019-10-15 16:22:16 +0200

    correct spacing problem 2_partial.py

--- workflow/2_partial.py
+++ workflow/2_partial.py
@@ -6,17 +6,11 @@ from lebor import *
 try:
     part = Partial('D_Chen_partial.bin.tsv')
 except:
-    part = Partial('D_Chen_subset.tsv', segments = 'tokens')
-    part.get_partial_scorer(runs = 9999)
-    part.output('tsv', filename= 'D_Chen_partial.bin', ignore=[],prettify = False)
+    part = Partial('D_Chen_subset.tsv', segments='tokens')
+    part.get_partial_scorer(runs=10000)
+    part.output('tsv', filename='D_Chen_partial.bin', ignore=[], prettify=False)
     print('save saved the scorer, load again to do more')
 finally:
-    part.partial_cluster(method = 'lexstat', threshold = 0.55)
-
-
-
+    part.partial_cluster(method='lexstat', threshold=0.55, ref='cogids')
 
 part.output('tsv', filename='D_Chen_partial', prettify=False)
-
-# Should I download the data from edictor? 
-# the new algorithm generate a new column "partial_cognate_sets". Should I convert this to cogid?
