41e61bc5ac38e60133f7c87415ed5637a47e7f8c
MacyL <wumeishin@gmail.com>
2019-10-16 15:05:07 +0200

    editing alignment.py

--- workflow/3_alignment.py
+++ workflow/3_alignment.py
@@ -1,18 +1,23 @@
 from sinopy import segments
 from lingpy import *
-from lingrex import *
+from align import align_to_template
+#from lingrex.util import align_by_structure
 
 
-wl = Wordlist('D_Chen_partial.tsv')
-
+wl=Wordlist('D_Chen_partial.tsv')
 wl.add_entries('structure', 'tokens', lambda x: list(segments.get_structure(x)))
+template_alignment(wl,
+                   ref='cogids',
+                   template='imnct_imnct_imnct',
+                   structure = 'structure',
+                   fuzzy=False,
+                   segments='tokens')
 
-# print out for inspection
-for idx, cp, tok, structure in wl.iter_rows('concept', 'tokens', 'structure'):
-    print(wl[idx, 'doculect'], cp, tok, structure)
 
-# alignment
-
-# output
 
 wl.output('tsv', filename='D_Chen_structure', ignore=[], prettify=False)
+
+
+# print out for inspection
+#for idx, cp, tok, structure in wl.iter_rows('concept', 'tokens', 'structure'):
+#    print(wl[idx, 'doculect'], cp, tok, structure)
