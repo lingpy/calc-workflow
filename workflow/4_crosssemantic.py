641e49381b7b8d2f1d38f678f0ffb027f1eb09a7
lingulist <mattis.list@lingpy.org>
2019-11-29 17:31:29 +0100

    update code

--- workflow/4_crosssemantic.py
+++ workflow/4_crosssemantic.py
@@ -1,7 +1,8 @@
 from lingpy import *
-from lingrex.colex import find_colexified_alignments
+from lingrex.colex import find_colexified_alignments, find_bad_internal_alignments
 
 alms = Alignments('D_Chen_aligned.tsv', ref='cogids')
+find_bad_internal_alignments(alms)
 
 find_colexified_alignments(
         alms,
