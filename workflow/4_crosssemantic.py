9227029c94c3ad98c35476c9d46fe0830e46a3e1
lingulist <mattis.list@lingpy.org>
2020-03-08 18:45:06 +0100

    update code to account for re-alignment

--- workflow/4_crosssemantic.py
+++ workflow/4_crosssemantic.py
@@ -1,5 +1,6 @@
 from lingpy import *
 from lingrex.colex import find_colexified_alignments, find_bad_internal_alignments
+from lingrex.align import template_alignment
 
 alms = Alignments('D_Chen_aligned.tsv', ref='cogids')
 find_bad_internal_alignments(alms)
@@ -10,5 +11,13 @@ find_colexified_alignments(
         segments='tokens',
         ref='crossids'
         )
+
+# re-align the data
+template_alignment(alms,
+                   ref='crossids',
+                   template='imnct+imnct+imnct+imnct+imnct+imnct',
+                   structure = 'structure',
+                   fuzzy=True,
+                   segments='tokens')
 
 alms.output('tsv', filename='D_Chen_crossids', prettify=False)
