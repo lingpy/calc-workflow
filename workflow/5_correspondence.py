ada576158eeff987a3cd61b504852e312b00c6f0
MeiShinWu <MacyL@users.noreply.github.com>
2020-03-17 13:27:19 +0100

    add statistics

--- workflow/5_correspondence.py
+++ workflow/5_correspondence.py
@@ -12,3 +12,19 @@ cop.sites_to_pattern()
 cop.add_patterns()
 cop.write_patterns('D_patterns_Chen.tsv')
 cop.output('tsv', filename='D_Chen_patterns', prettify=False)
+
+# statistics 
+sps=['i','m','n','c','t']
+
+total_correspondence_sets = len(cop.clusters)
+print('{0}: {1}'.format('The total sound correspondence cluster sets', total_correspondence_sets))
+
+print('The number of regular correspondence sets in each position')
+for sp in sps:
+    t = [x[1] for x, y in cop.clusters.items() if len(y)>1 and x[0] ==sp]
+    print('{0}: {1}'.format(sp, len(t)))
+
+print('The number of singletons in each position ')
+for sp in sps:
+    t = [x[1] for x, y in cop.clusters.items() if len(y)==1 and x[0] ==sp]
+    print('{0}: {1}'.format(sp, len(t)))
