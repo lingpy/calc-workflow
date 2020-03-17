0f71f5a6112c54e98cf9646f352512ec10d85ccc
MeiShinWu <MacyL@users.noreply.github.com>
2020-03-17 09:03:47 +0100

    remove or change file names

--- workflow/P_validation.py
+++
@@ -1,9 +0,0 @@
-from lingpy import Wordlist
-from lingpy.basic import ops
-
-# read in data
-wl = Wordlist('D_Chen_crossids.tsv')
-# calculate the distance via shared cognates
-wl.calculate('dst', taxa='doculect', concepts='concepts', ref='crossids')
-# output
-wl.output('dst', filename='D_Chen_distance')
