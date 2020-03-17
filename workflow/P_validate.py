b2312c6da4c2710de7d39edc0d0f1c60faa78e47
MeiShinWu <MacyL@users.noreply.github.com>
2020-03-17 13:27:44 +0100

    print cognate density

--- workflow/P_validate.py
+++ workflow/P_validate.py
@@ -1,15 +1,18 @@
 import argparse
 from lingpy import Wordlist
 from lingpy.basic import ops
+from lingrex.copar import density
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', help='input file')
 parser.add_argument('--column')
-test = "--input test --column cogids"
 args = parser.parse_args()
 #print(args) 
 wl = Wordlist(args.input)
 
+# cognate density 
+cdensity = density(wl, ref=args.column)
+print('The cognate density ')
 # calculate the distance via shared cognates
 wl.calculate('dst', taxa='doculect', concepts='concepts', ref=args.column)
 
